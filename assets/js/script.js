0bd2e00969024e0583687ed86b7c416e4b64f736
justmartin <justin.lamar.martin@gmail.com>
2016-09-14 14:20:25 -0700

    Added shoe images and initial modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,24 @@ $( document ).ready(function() {
 	$('.carousel.carousel-slider').carousel({full_width: true});
 	$(".button-collapse").sideNav();
 });
+
+
+
+//CODE FOR MODAL
+
+document.getElementById("page-body").addEventListener("load", function(event){
+	setTimeout(modalPopup, 3000);
+});
+
+var modalPopup = function(){
+	// Get the modal
+	var modal = document.getElementById('newsletter-modal');
+	// Get the <span> element that closes the modal
+	var span = document.getElementsByClassName("close")[0];
+	//displays the Modal
+	modal.style.display = "block";
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+	modal.style.display = "none";
+	}
+}
